3d3624012fca3dcbab1de3d2b9902038d192314c
cyrus <cyli@idahtechs.com>
2024-10-23 16:46:56 +0800

    适配文案提取/生成模块

--- uni-app/src/api/ai.js
+++ uni-app/src/api/ai.js
@@ -1,41 +1,41 @@
 import request from "@/utils/request.js";
 
 // 视频脚本提取
-export const extractVideoScript = (data) => {
+export const extractContent = (data) => {
   return request.post('ai/tools/extractCopyByUrl', data)
 }
 
 // 仿写视频脚本
-export const generateVideoScript = (data) => {
+export const generateContent = (data) => {
   return request.post('ai/tools/script/initiation ', data)
 }
 
 // 重新仿写视频脚本
-export const regenerateVideoScript = (id,data) => {
+export const regenerateContent = (id,data) => {
   return request.post(`ai/tools/script/recreate/${id}`, data)
 }
 
 // 删除视频脚本
-export const deleteVideoScript = (id) => {
+export const deleteContent = (id) => {
   return request.delete(`ai/tools/script/delete/${id}`)
 }
 
 // 删除视频脚本记录项
-export const deleteVideoScriptVersion = (id) => {
+export const deleteContentVersion = (id) => {
   return request.delete(`ai/tools/script/version/delete/${id}`)
 }
 
 // 视频脚本润色
-export const polishVideoScript = (id, data) => {
+export const polishContent = (id, data) => {
   return request.post(`ai/tools/script/polish/${id}`, data)
 }
 
 // 视频脚本创作详情
-export const getVideoScriptDetail = (id) => {
+export const getContentDetail = (id) => {
   return request.get(`ai/tools/script/detail/${id}`)
 }
 
 // 视频脚本创作列表
-export const getVideoScriptList = (data) => {
+export const getContentList = (data) => {
   return request.get('ai/tools/script/lst', data)
 }
--- uni-app/src/pages/index/index.vue
+++ uni-app/src/pages/index/index.vue
@@ -7,10 +7,10 @@
     <image src="/static/images/bg-index.png" class="bg" mode="widthFix" />
 
     <view class="entries">
-      <view class="entry" v-for="entry in entries" :key="entry.title">
+      <view class="entry" v-for="entry in entries" :key="entry.group_data_id">
         <image class="entry-cover" :src="entry.cover" mode="aspectFill" />
         <view class="entry-info">
-          <view class="fs-16 font-bold">{{ entry.title }}</view>
+          <view class="fs-16 font-bold">{{ entry.name }}</view>
           <view class="fs-16">{{ entry.desc }}</view>
           <button class="cmi-btn cmi-btn-sm entry-btn" type="primary" @click="goTo(entry)">{{ entry.buttonText }}</button>
         </view>
@@ -20,18 +20,12 @@
 </template>
 
 <script>
+import { getConfigGroupWithCache } from '@/api/public';
+
 export default {
   data() {
     return {
-      entries: [
-        {
-          title: '视频变种',
-          desc: '创意内容生成',
-          cover: '/static/images/cover-bzgc.png',
-          url: '/pages/ai/video_script_extract/index',
-          buttonText: '开始写作'
-        }
-      ]
+      entries: []
     }
   },
 
@@ -41,10 +35,32 @@ export default {
     }
   },
 
+  onPullDownRefresh() {
+    this.loadEntries(true).finally(() => {
+      uni.stopPullDownRefresh()
+    })
+  },
+
+  onLoad() {
+    this.loadEntries()
+  },
+
   methods: {
+    loadEntries(force) {
+      const request = force ? getConfigGroupWithCache.reload : getConfigGroupWithCache
+
+      return request('mobile_home_entries').then(res => {
+        this.entries = res.data
+      }).catch(err => {
+        uni.showToast({
+          title: err.message,
+          icon: 'none'
+        })
+      })
+    },
     goTo(entry) {
       this.$util.gotoPage({ url: entry.url })
-    }
+    },
   }
 }
 </script>
@@ -60,7 +76,7 @@ export default {
 .entries {
   display: flex;
   flex-direction: column;
-  gap: 32rpx;
+  gap: 48rpx;
   padding: 226rpx 40rpx 40rpx;
 }
 
